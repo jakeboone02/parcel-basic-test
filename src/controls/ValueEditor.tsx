d049da5861a9cff19759a80d01cb642b64e0fd94
Jake Boone <jakeboone02@gmail.com>
2021-11-24 10:00:53 -0700

    Indicated working code change

--- src/controls/ValueEditor.tsx
+++ src/controls/ValueEditor.tsx
@@ -1,3 +1,6 @@
+// This works
+// import type { ValueEditorProps } from "../types/props";
+// This does not work
 import type { ValueEditorProps } from "../types";
 
 export const ValueEditor = ({ someValue }: ValueEditorProps) => {
